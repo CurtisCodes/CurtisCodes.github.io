88eac653ebfc687f0f08bcfb39025129caef61aa
Curtis Perdue <cwperdue@outlook.com>
2023-06-20 22:13:27 -0600

    added weather app and changed style

---
+++ js/app.js
@@ -0,0 +1,72 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+    // non-destructured properties
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    // destructured properties
+    const { cityDets, weather } = data;
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}, ${cityDets.AdministrativeArea.ID}</h5>
+        <h4 class="my-2 fs-2">${cityDets.Country.LocalizedName}</h4>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Imperial.Value}</span>
+            <span>&deg;F</span>
+        </div>
+    `;
+
+    //update the night/day & icon images
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    // using a ternary object instead of an if statement
+    // let timeSrc = null;
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg';
+    // }
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    };
+
+
+    console.log(data);
+
+};
+
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return { cityDets, weather };
+};
+
+cityForm.addEventListener('submit', e =>{
+    //prevent default action
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+});
