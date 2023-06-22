9e2c1fba1965fd0a228a1e59bd97ffabc2fc795e
Curtis Perdue <cwperdue@outlook.com>
2023-06-21 20:00:18 -0600

    made the weather app hidden

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+const weatherButton = document.querySelector('section button');
+const container = document.querySelector('.container');
 
+weatherButton.addEventListener('click', e => {
+    e.preventDefault();
+    if(container.style.display == 'none'){
+        container.style.display = 'block';
+    } else {
+        container.style.display = 'none';
+    };
+});
